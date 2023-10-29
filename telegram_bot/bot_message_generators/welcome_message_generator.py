a6d8793b5ac6dd671c81dc2cc9dd25d21ead5530
AlexSkvorz <alexskvorz@gmail.com>
2023-10-29 16:16:40 +0300

    add users to database

--- telegram_bot/bot_message_generators/welcome_message_generator.py
+++ telegram_bot/bot_message_generators/welcome_message_generator.py
@@ -1,2 +1,2 @@
 async def send_welcome_message(bot, chat_id, username):
-    await bot.send_message(chat_id, f'Привет, {username} я асинхронный пидор!')
+    await bot.send_message(chat_id, f'Привет, {username}, я Бот-Викторина!')
