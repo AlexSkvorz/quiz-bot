e0a103586b246663282d2e27c43069b0451e43b8
AlexSkvorz <alexskvorz@gmail.com>
2023-10-29 16:16:40 +0300

    add users to database

--- database/users_table.py
+++ database/users_table.py
@@ -11,3 +11,12 @@ async def create_table():
             role INTEGER)
             ''')
         await db.commit()
+
+
+async def insert_user(user_id, username, role):
+    async with aiosqlite.connect(STORAGE_CONFIG['DB_NAME']) as db:
+        await db.execute('''
+        INSERT OR IGNORE INTO users_table (user_id, username, role) VALUES (?, ?, ?)
+        ''', parameters=(user_id, username, role)
+                         )
+        await db.commit()
--- telegram_bot/bot_handlers/handle_start_command.py
+++ telegram_bot/bot_handlers/handle_start_command.py
@@ -1,6 +1,10 @@
 from telegram_bot.bot_message_generators.welcome_message_generator import send_welcome_message
+from database.users_table import insert_user
 
 
 async def handle_start_command(bot, message):
+    user_id = message.from_user.id
     username = message.from_user.username
+
+    await insert_user(user_id=user_id, username=username, role='user')
     await send_welcome_message(bot=bot, chat_id=message.chat.id, username=username)
