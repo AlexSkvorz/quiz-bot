9fdb3f344053c0c6b363b69d90e6ae878cca7a57
AlexSkvorz <alexskvorz@gmail.com>
2023-11-01 14:06:33 +0300

    refactor handlers structure

--- telegram_bot/bot_handlers/handle_send_question.py
+++ telegram_bot/bot_handlers/handle_send_question.py
@@ -1,5 +1,6 @@
 from database.questions_table import fetch_unique_question
 from telegram_bot.bot_message_generators.quiz_question_generator import send_quiz_question
+from telegram_bot.bot_message_generators.end_of_quiz_message_generator import send_out_questions
 import json
 
 
@@ -8,17 +9,26 @@ async def handle_send_question(bot, call):
 
     unique_question = await fetch_unique_question(user_id=call.from_user.id, topic=topic)
 
-    quiz_id = unique_question[0][0]
-    question = unique_question[0][1]
-    answers = json.loads(unique_question[0][2])
-    correct_answer = unique_question[0][3]
+    try:
+        quiz_id = unique_question[0][0]
+        question = unique_question[0][1]
+        answers = json.loads(unique_question[0][2])
+        correct_answer = unique_question[0][3]
 
-    await send_quiz_question(
-        bot=bot,
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        quiz_id=quiz_id,
-        question=question,
-        answers=answers,
-        correct_answer=correct_answer
-    )
+        await send_quiz_question(
+            bot=bot,
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            quiz_id=quiz_id,
+            question=question,
+            answers=answers,
+            correct_answer=correct_answer
+        )
+
+    except IndexError:
+        await send_out_questions(
+            bot=bot,
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            username=call.from_user.username
+        )
