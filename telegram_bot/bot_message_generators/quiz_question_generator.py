d3d572d2dddf049a151e3b5b438547207170b371
AlexSkvorz <alexskvorz@gmail.com>
2023-11-07 18:36:41 +0300

    change question callback-data

--- telegram_bot/bot_message_generators/quiz_question_generator.py
+++ telegram_bot/bot_message_generators/quiz_question_generator.py
@@ -5,8 +5,7 @@ from telegram_bot.bot_message_generators.create_inline_keyboard import create_in
 async def send_quiz_question(bot, chat_id, message_id, quiz_id, question, answers, correct_answer):
     message = f'{question}'
 
-    button_parameters = {answer: f'select_answer_{answer}_quiz_id={quiz_id}' for answer in answers}
-
+    button_parameters = {answer: f'select_answer_quiz_id={quiz_id}' for answer in answers}
     button_parameters[correct_answer] += f'_correct'
 
     markup = create_inline_keyboard(button_parameters=button_parameters)
