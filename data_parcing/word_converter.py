fe3e46282f8d4eae52692f81fa8cbc0aaa5da911
Eugene <zhenya.mikhailov.02@mail.ru>
2023-11-08 18:25:03 +0300

    pr remarks

--- data_parcing/word_converter.py
+++ data_parcing/word_converter.py
@@ -6,5 +6,3 @@ morph = pymorphy2.MorphAnalyzer()
 def get_word_form(quantity_unique_questions):
     word_form = morph.parse("вопросы")[0].make_agree_with_number(quantity_unique_questions).word
     return word_form
-
-
